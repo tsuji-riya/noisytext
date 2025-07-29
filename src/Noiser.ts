e362f2331a0c6708bed7290a2f660cfaa013da81
tsuji-riya <tsuji.riya@gmail.com>
2025-07-29 15:59:10 +0900

    ✨️ Initial commit

---
+++ src/Noiser.ts
@@ -0,0 +1,44 @@
+export class Noiser {
+  private static randomize(value: string): string {
+    return value
+      .split("")
+      .map((char) => {
+        if (char.match(/[0-9a-zA-Z]/)) {
+          return String.fromCharCode(
+            Math.floor(Math.random() * 26) + 97, // Random lowercase letter (ASCII 97-122)
+          );
+        } else if (char.match(/[\s]/)) {
+          return " "; // Keep spaces as they are
+        } else {
+          return String.fromCharCode(
+            Math.floor(Math.random() * 15) + 33, // Random symbol (ASCII 33-47)
+          );
+        }
+      })
+      .join("");
+  }
+
+  constructor(private readonly period: number = 50) {}
+
+  public start(value: string, callback: (chars: string) => void) {
+    if (value === "" || value === null || value === undefined) return;
+    let index = 1;
+    let chars = value[0];
+    const maxIndex = value.length;
+    const interval = setInterval(() => {
+      if (index >= maxIndex + 10) {
+        clearInterval(interval);
+        callback(value);
+        return;
+      }
+      if (index < maxIndex) {
+        chars += value[index];
+      }
+      callback(
+        chars.substring(0, Math.max(0, index - 10)) +
+          Noiser.randomize(chars.substring(Math.max(0, index - 10), index)),
+      );
+      index += 1;
+    }, this.period);
+  }
+}
